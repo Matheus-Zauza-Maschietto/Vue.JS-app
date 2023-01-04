ad6f4383c1eb826cbb4244fb5384a17f9d49582a
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-01-04 11:02:07 -0300

    update todolist

---
+++ projectVue/to-do-list/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="toDo">
+      <h1>Minhas Tarefas</h1>
+      <div class="tasks">
+        <newTask @valorTask="taskList.push($event)" />
+        <div>
+          <p v-for="task in taskList" :key="task">{{task}}</p>
+        </div>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+import newTask from "./components/newTask.vue"
+
+export default {
+  name: 'App',
+  components:{
+    newTask
+  },
+  data(){
+    return{
+      taskList: []
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+  #toDo{
+    width: 40vw;
+    height: clamp(400px, auto, auto);
+    margin: 0 auto;
+  }
+  h1{
+    color: white;
+    font-size: 25px;
+    padding: 10px 0 20px 0;
+    text-align: center;
+  }
+  .tasks{
+    background-color: white;
+  }
+</style>
+
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .flex-center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #app{
+    background-color: #07525A;
+    width: 100vw;
+    min-height: 100vh;
+  }
+</style>
+
---
+++ projectVue/to-do-list/src/components/newTask.vue
@@ -0,0 +1,37 @@
+<template>
+    <form class="flex-center">
+        <input type="text" v-model="taskInfo" required placeholder="Tarefa de hoje?">
+        <button @click.prevent="PassarValorTaskAdicionar()">Adicionar</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "newTask",
+    data(){
+        return{
+            taskInfo: ""
+        }
+    },
+    methods: {
+        PassarValorTaskAdicionar(){
+            this.$emit("valorTask", this.taskInfo)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form{
+        flex-direction: row;
+        margin: 0 auto;
+        padding: 15px 10px
+    }
+    input{
+        width: clamp(300px, 80%, 500px);
+    }
+    button{
+        color: white;
+        background-color: #07525A;
+    }
+</style>
